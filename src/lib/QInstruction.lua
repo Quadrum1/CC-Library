e1399086975e3393e84eb1c0225428a11dc03377
Quadrum <38959426+Quadrum1@users.noreply.github.com>
2023-06-01 20:42:29 +0200

    Initial

---
+++ src/lib/QInstruction.lua
@@ -0,0 +1,26 @@
+-- Version: 0.1
+local Public = {}
+
+
+Public.QInstructionCalls = {
+        ["up"] = turtle.up,
+        ["down"] = turtle.down,
+        ["forward"] = turtle.forward,
+        ["backwards"] = turtle.back,
+        ["left"] = turtle.turnLeft,
+        ["right"] = turtle.turnRight
+}
+
+function Public.execute(command)
+    if Public.calls[command] then
+        Public.calls[command]()
+    end
+end
+
+function Public.executeSet(instructionSet)
+    for i=1, #instructionSet, 1 do 
+        Public.execute(instructionSet[i])
+    end
+end
+
+return Public
---
+++ src/qmanager.lua
@@ -0,0 +1,79 @@
+
+-- Fetches latest version of module from github.
+-- All modules are backwards compatible, unless specified.
+
+local args = {...}
+
+
+local keywords = {
+    ["install"] = true,
+    ["update"] = true
+}
+
+if #args < 2 or not keywords[args[1]] then
+    io.write("Usage:\n")
+    -- Install will ensure version and update when version is less than specified
+    io.write("qmanager install <package> [v]\n")
+    
+    -- Update will update package to latest version
+    io.write("qmanager update <package> [v]\n")
+    return nil
+end
+
+local packages = {
+    ["QMovement"] = {
+        name = "QMovement.lua",
+        description = "Basic movement package",
+        path = "",
+        type = "library"
+    },
+    ["QInstruction"] = {
+        name = "QInstruction.lua",
+        description = "Allows executing several commands as a string",
+        path = "",
+        type = "library"
+    }
+}
+
+if not packages[args[2]] then
+    io.write("Invalid package ["..args[2].."] requested\n")
+    return nil
+end
+
+local function install(package, target_version)
+    local response = http and http.get(package.path)
+    if not response then
+        error("Could not fetch " .. package.name)
+    end
+    
+    if package.type == "library" then
+        local path = "/QLib/packages/"
+    else
+        local path = "/"
+    end
+    
+    if fs.exists(path .. package.name) and target_version then
+        local f = io.open(path .. package.name)
+        local line = f:read()
+        f:close()
+        line = string.gsub(line, "[%a%p%s]", "") -- Only leave digits
+        
+        if tonumber(line) >= target_version then
+            return
+        end
+    end
+    
+    local handle = io.open(path .. package.name, "w")
+    if handle then
+        handle:write(response.readAll())
+        handle:flush()
+        handle:close()
+    else
+        error("Could not write ".. path .. package.name)
+    end
+    
+    io.write("Successfully installed " .. package.name)
+    return nil
+end
+
+install(packages[args[2]], args[3])
